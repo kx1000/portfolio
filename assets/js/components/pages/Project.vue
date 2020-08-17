6ee8752c1661219db18fc4aca86a8cbdf2757305
Kacper <kacper.rogula@gmail.com>
2020-08-17 18:44:27 +0200

    Project page

--- assets/js/components/pages/Project.vue
+++ assets/js/components/pages/Project.vue
@@ -1,10 +1,22 @@
 <template>
 <div>
-  <router-link to="/projects">< powrót</router-link>
+  <router-link to="/projects" class="back">< powrót</router-link>
   <h1>
     <vue-typer text="Projekt 1" :repeat="0"></vue-typer>
   </h1>
-  :)
+  <div class="project-grid">
+    <div>
+      <img src="/assets/img/Cakeshop-compressed.gif">
+    </div>
+    <div>
+      Prosty system e-commerce.
+      [opisać funkcje]
+      [opisać panel admina]
+      Projekt wykonany na potrzeby pracy inżynierskiej.
+      Mój pierwszy projekt w Symfony.
+    </div>
+  </div>
+
   <h2>{{ $route.params.slug }}</h2>
 </div>
 </template>
@@ -14,3 +26,19 @@ export default {
   name: "Project"
 }
 </script>
+
+<style>
+.project-grid {
+  align-items: center;
+  display: grid;
+  grid-template-rows: auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-rows: auto;
+  grid-template-columns: repeat( auto-fit, minmax(calc(var(--page-width) / 3), 1fr) );
+}
+
+.back {
+  float: right;
+}
+</style>
