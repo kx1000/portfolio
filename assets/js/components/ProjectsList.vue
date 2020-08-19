885c625277533b6dea2e54c9b0ade170b8a56741
Kacper <kacper.rogula@gmail.com>
2020-08-19 12:51:38 +0200

    Install axios

--- assets/js/components/ProjectsList.vue
+++ assets/js/components/ProjectsList.vue
@@ -3,21 +3,29 @@
     <h1>
       <vue-typer text="Projekty" :repeat="0"></vue-typer>
     </h1>
-    <div class="image-grid">
-      <router-link to="/projects/asd">jakiś projekt</router-link>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;1" alt="random image" width="auto" height="auto">asd</a>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;2" alt="random image" width="auto" height="auto">dasdasdasdas asd sad asdasd </a>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;3" alt="random image" width="auto" height="auto">kodaskosdakoasd asokads</a>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;4" alt="random image" width="auto" height="auto">sad  asddsa</a>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;5" alt="random image" width="auto" height="auto">sdaasd dsd</a>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;6" alt="random image" width="auto" height="auto">asdsad</a>
-      <a href="https://picsum.photos" style="border: none;"><img src="https://picsum.photos/200/300?random&amp;7" alt="random image" width="auto" height="auto">żźż®ęłę∑Ō</a>
+    <div v-for="project in projects" class="image-grid">
+      <router-link :to="'/projects/' + project.slug">
+        <img v-if="project.image" :src="project.image" :alt="project.title" width="auto" height="auto">
+        {{ project.title }}
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "ProjectsList",
+  data() {
+    return {
+      projects: null,
+    }
+  },
+  mounted() {
+    axios.get('/api/projects.json')
+    .then(res => this.projects = res.data)
+    .catch(error => console.log(error))
+  }
 }
 </script>
