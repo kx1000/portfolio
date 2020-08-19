cb05c699d4f1bbf3163189ad7d81093e5d14a5d8
Kacper <kacper.rogula@gmail.com>
2020-08-19 13:27:54 +0200

    Login to admin panel

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+             return $this->redirectToRoute('admin');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+            'translation_domain' => 'admin',
+            'page_title' => 'Admin',
+            'csrf_token_intention' => 'authenticate',
+            'target_path' => $this->generateUrl('admin'),
+            'username_label' => 'Your email',
+            'password_label' => 'Your password',
+            'sign_in_label' => 'Log in',
+            'username_parameter' => 'email',
+            'password_parameter' => 'password',
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
