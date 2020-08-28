82b716b1226faf93cc285cf3216ddcfda9a242ef
Kacper <kacper.rogula@gmail.com>
2020-08-28 21:42:02 +0200

    Loading bar

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,33 +1,40 @@
 <template>
   <div id="app" class="terminal">
-    <div class="container">
-      <div class="terminal-nav">
-        <div class="terminal-logo">
-          <div class="logo terminal-prompt">
-            <router-link class="no-style" to="/">
-              {{ header.title }}
-            </router-link>
+    <div v-if="actionsCount === contentsLoadedCount">
+      <div class="container">
+        <div class="terminal-nav">
+          <div class="terminal-logo">
+            <div class="logo terminal-prompt">
+              <router-link class="no-style" to="/">
+                {{ header.title }}
+              </router-link>
+            </div>
+            {{ header.body }}
           </div>
-          {{ header.body }}
+          <nav class="terminal-menu">
+            <ul>
+              <li><dark-mode-switch /></li>
+              <li><router-link to="/" exact>O mnie</router-link></li>
+              <li><router-link to="/projects">Projekty</router-link></li>
+              <li><router-link to="/contact">Kontakt</router-link></li>
+            </ul>
+          </nav>
         </div>
-        <nav class="terminal-menu">
-          <ul>
-            <li><dark-mode-switch /></li>
-            <li><router-link to="/" exact>O mnie</router-link></li>
-            <li><router-link to="/projects">Projekty</router-link></li>
-            <li><router-link to="/contact">Kontakt</router-link></li>
-          </ul>
-        </nav>
+      </div>
+      <div class="container">
+        <hr>
+        <transition :name="transitionName" mode="out-in">
+          <router-view></router-view>
+        </transition>
+        <hr>
+        <div class="center">
+          {{ footer.title }} <span v-html="footer.body"/>
+        </div>
       </div>
     </div>
-    <div class="container">
-      <hr>
-      <transition :name="transitionName" mode="out-in">
-        <router-view></router-view>
-      </transition>
-      <hr>
-      <div class="center">
-        {{ footer.title }} <span v-html="footer.body"/>
+    <div v-else>
+      <div class="progress-bar progress-bar-no-arrow">
+        <div class="progress-bar-filled" :style="getLoadingProgress()"></div>
       </div>
     </div>
   </div>
@@ -35,8 +42,8 @@
 
 <script>
 import DarkModeSwitch from "./components/DarkModeSwitch";
-import ContentObject from "./Object/ContentObject";
 import {mapState} from 'vuex'
+import {actions} from "./store";
 
 const NEXT_TRANSITION = 'slide-left';
 const PREVIOUS_TRANSITION = 'slide-right';
@@ -46,8 +53,14 @@ export default {
   components: { DarkModeSwitch },
   data () {
     return {
+      actionsCount: null,
       transitionName: NEXT_TRANSITION,
-      footerData: new ContentObject(),
+    }
+  },
+  methods: {
+    getLoadingProgress: function () {
+      let progress = (this.contentsLoadedCount * 100) / this.actionsCount;
+      return "width: " + progress + "%";
     }
   },
   watch: {
@@ -58,18 +71,17 @@ export default {
     }
   },
   mounted() {
-    this.$store.dispatch('loadHeader')
-    this.$store.dispatch('loadFooter')
-    this.$store.dispatch('loadAbout')
-    this.$store.dispatch('loadProjectsList')
-    this.$store.dispatch('loadProjects')
-    this.$store.dispatch('loadContact')
-    this.$store.dispatch('loadSentConfirmation')
+    this.actionsCount = Object.keys(actions).length;
+
+    for (const action in actions) {
+      this.$store.dispatch(action);
+    }
   },
   computed: {
     ...mapState([
         'header',
-        'footer'
+        'footer',
+        'contentsLoadedCount'
     ])
   }
 }
--- assets/js/store.js
+++ assets/js/store.js
@@ -5,8 +5,68 @@ import {contentNames, ApiService} from './Service/ApiService'
 
 Vue.use(vuex, axios)
 
+export const actions = {
+    loadHeader({commit}) {
+        ApiService
+            .fetchContent(contentNames.HEADER)
+            .then(data => {
+                commit('SET_HEADER', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    },
+    loadFooter({commit}) {
+        ApiService
+            .fetchContent(contentNames.FOOTER)
+            .then(data => {
+                commit('SET_FOOTER', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    },
+    loadAbout({commit}) {
+        ApiService
+            .fetchContent(contentNames.ABOUT)
+            .then(data => {
+                commit('SET_ABOUT', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    },
+    loadProjectsList({commit}) {
+        ApiService
+            .fetchContent(contentNames.PROJECTS)
+            .then(data => {
+                commit('SET_PROJECTS_LIST', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    },
+    loadContact({commit}) {
+        ApiService
+            .fetchContent(contentNames.CONTACT)
+            .then(data => {
+                commit('SET_CONTACT', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    },
+    loadSentConfirmation({commit}) {
+        ApiService
+            .fetchContent(contentNames.SENT)
+            .then(data => {
+                commit('SET_SENT_CONFIRMATION', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    },
+    loadProjects({commit}) {
+        ApiService
+            .fetchProjects()
+            .then(data => {
+                commit('SET_PROJECTS', data)
+                commit('INCREMENT_CONTENTS_LOADED_COUNT')
+            })
+    }
+};
+
 export default new vuex.Store({
     state: {
+        contentsLoadedCount: 0,
         header: '',
         footer: '',
         about: '',
@@ -15,43 +75,7 @@ export default new vuex.Store({
         sentConfirmation: '',
         projects: [],
     },
-    actions: {
-        loadHeader({commit}) {
-            ApiService
-                .fetchContent(contentNames.HEADER)
-                .then(data => commit('SET_HEADER', data))
-        },
-        loadFooter({commit}) {
-            ApiService
-                .fetchContent(contentNames.FOOTER)
-                .then(data => commit('SET_FOOTER', data))
-        },
-        loadAbout({commit}) {
-            ApiService
-                .fetchContent(contentNames.ABOUT)
-                .then(data => commit('SET_ABOUT', data))
-        },
-        loadProjectsList({commit}) {
-            ApiService
-                .fetchContent(contentNames.PROJECTS)
-                .then(data => commit('SET_PROJECTS_LIST', data))
-        },
-        loadContact({commit}) {
-            ApiService
-                .fetchContent(contentNames.CONTACT)
-                .then(data => commit('SET_CONTACT', data))
-        },
-        loadSentConfirmation({commit}) {
-            ApiService
-                .fetchContent(contentNames.SENT)
-                .then(data => commit('SET_SENT_CONFIRMATION', data))
-        },
-        loadProjects({commit}) {
-            ApiService
-                .fetchProjects()
-                .then(data => commit('SET_PROJECTS', data))
-        }
-    },
+    actions: actions,
     mutations: {
         SET_HEADER (state, data) {
             state.header = data
@@ -73,6 +97,9 @@ export default new vuex.Store({
         },
         SET_PROJECTS (state, data) {
             state.projects = data
+        },
+        INCREMENT_CONTENTS_LOADED_COUNT (state) {
+            state.contentsLoadedCount++;
         }
     }
 })
