b6bfe137f42af67b72e4dc0731e74acc56cd4bff
Kacper <kacper.rogula@gmail.com>
2020-09-20 14:53:02 +0200

    Set arguments as optional in create user command

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -5,9 +5,12 @@ namespace App\Command;
 use App\Factory\UserFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -38,16 +41,29 @@ class AddUserCommand extends Command
     protected function configure()
     {
         $this
-            ->addArgument('email', InputArgument::REQUIRED)
-            ->addArgument('password', InputArgument::REQUIRED)
+            ->addArgument('email', InputArgument::OPTIONAL)
+            ->addArgument('password', InputArgument::OPTIONAL)
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $errorIo = $output instanceof ConsoleOutputInterface ? new SymfonyStyle($input, $output->getErrorOutput()) : $io;
         $email = $input->getArgument('email');
         $password = $input->getArgument('password');
+
+        $errorIo->title('Creating a new user');
+
+        if (!$email) {
+            $emailQuestion = $this->createEmailQuestion();
+            $email = $errorIo->askQuestion($emailQuestion);
+        }
+
+        if (!$password) {
+            $passwordQuestion = $this->createPasswordQuestion();
+            $password = $errorIo->askQuestion($passwordQuestion);
+        }
 
         $user = $this->userFactory->createUser($email, $password);
 
@@ -64,8 +80,34 @@ class AddUserCommand extends Command
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        $io->success('Utworzono użytkownika :)');
+        $io->success('User '. $email .' has been created :)');
 
         return 0;
+    }
+
+    private function createEmailQuestion(): Question
+    {
+        $emailQuestion = new Question('Type in email');
+
+        return $emailQuestion->setValidator(function ($value) {
+            if ('' === trim($value)) {
+                throw new InvalidArgumentException('An email must not be empty.');
+            }
+
+            return $value;
+        })->setMaxAttempts(20);
+    }
+
+    private function createPasswordQuestion(): Question
+    {
+        $passwordQuestion = new Question('Type in password');
+
+        return $passwordQuestion->setValidator(function ($value) {
+            if ('' === trim($value)) {
+                throw new InvalidArgumentException('The password must not be empty.');
+            }
+
+            return $value;
+        })->setHidden(true)->setMaxAttempts(20);
     }
 }
