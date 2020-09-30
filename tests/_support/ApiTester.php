bbaad9cc71d30900f7b1a11687020b597d5fb46d
Kacper <kacper.rogula@gmail.com>
2020-09-30 20:56:56 +0200

    Clean up tests directry

--- tests/_support/ApiTester.php
+++ tests/_support/ApiTester.php
@@ -12,15 +12,15 @@ namespace App\Tests;
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ * @method void pause()
  *
  * @SuppressWarnings(PHPMD)
 */
-class FunctionalTester extends \Codeception\Actor
+class ApiTester extends \Codeception\Actor
 {
-    use _generated\FunctionalTesterActions;
+    use _generated\ApiTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    /**
+     * Define custom actions here
+     */
 }
--- tests/_support/Helper/Api.php
+++ tests/_support/Helper/Api.php
@@ -4,7 +4,7 @@ namespace App\Tests\Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class Acceptance extends \Codeception\Module
+class Api extends \Codeception\Module
 {
 
 }
