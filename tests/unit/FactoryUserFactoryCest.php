eac7fbbb4af496c7c7477dfb75e288b0624a7fe6
Kacper <kacper.rogula@gmail.com>
2020-09-30 13:45:57 +0200

    Add user factory cest

---
+++ tests/unit/FactoryUserFactoryCest.php
@@ -0,0 +1,26 @@
+<?php namespace App\Tests\Factory;
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Tests\UnitTester;
+use Codeception\Stub\Expected;
+use Codeception\Util\Stub;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * php vendor/bin/codecept run unit Factory\UserFactoryCest
+ */
+class UserFactoryCest
+{
+    public function createUser(UnitTester $I)
+    {
+        $passwordEncoder = Stub::makeEmpty(UserPasswordEncoderInterface::class, [
+            'encodePassword' => 'test'
+        ]);
+        $factory = new UserFactory($passwordEncoder);
+        $user = $factory->createUser('test@test.com', 'test');
+
+        $I->assertInstanceOf(User::class, $user);
+        $I->assertEquals('test@test.com', $user->getEmail());
+        $I->assertNotEmpty($user->getPassword());
+    }
+}
