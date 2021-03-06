755f6fcb3968d99ce402255994eb24d68d45f1b3
Kacper <kacper.rogula@gmail.com>
2021-03-06 10:59:32 +0100

    Install alix bundle

---
+++ src/Field/TranslationField.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Field;
+
+
+use A2lix\TranslationFormBundle\Form\Type\TranslationsType;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+
+final class TranslationField implements FieldInterface
+{
+    use FieldTrait;
+
+    public static function new(string $propertyName, ?string $label = null, $fieldsConfig = []): self
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setFormType(TranslationsType::class)
+            ->setFormTypeOptions(
+                [
+                    'default_locale' => '%locale%',
+                    'fields' => $fieldsConfig,
+                ]
+            );
+    }
+}
