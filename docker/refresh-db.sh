0ab898993b357f00f93b4cbe24b62d13194af3ba
Kacper Rogula <kacper.rogula@gmail.com>
2024-11-24 14:51:21 +0100

    fix refresh-db.sh

--- docker/refresh-db.sh
+++ docker/refresh-db.sh
@@ -3,6 +3,5 @@
 sleep 1 &&
 php bin/console doctrine:database:create --if-not-exists &&
 php bin/console doctrine:schema:update --dump-sql --force --complete &&
-php bin/console doctrine:fixtures:load --no-interaction
 
 exec "$@"
